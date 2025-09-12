5a8ef63bf160bc37665f1abd0f7c677a3a88b870
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-09-11 22:10:12 -0600

    Agregando estilos al titulo Hola Mundo

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo!!!</Text>
+      <Text style={styles.holamundo}>Hola Mundo!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f1f3f3ff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  holamundo: {
+    fontSize: 50,
+    fontWeight: "600", // grosor de la letra
+    textShadowColor: "rgba(239, 11, 18, 0.5)", // color de la sombra/borde
+    textShadowOffset: { width: 2, height: 2 }, // dirección de la sombra
+    textShadowRadius: 3, // difuminado de la sombra
+    marginBottom: 10, // separación inferior
   },
 });
