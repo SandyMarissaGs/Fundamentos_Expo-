4064f4d60960e7e24383188a3a08ec9255b265a7
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-09-12 19:41:04 -0600

    Agregando boton con icono

--- src/Pages/Home/Botones.js
+++ src/Pages/Home/Botones.js
@@ -1,35 +1,62 @@
-import  Ionicons  from "@expo/vector-icons/FontAwesome";
-import { View, Text, StyleSheet, Platform, StatusBar, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/FontAwesome";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 
-export default function Botones () {
- return (
+export default function Botones() {
+  return (
     <View style={style.mainS}>
-        <Text style={style.h1}> Botones </Text>
-
-        <TouchableOpacity style={style.botonBasico}>
-          <Text> Boton 1</Text>
-        </TouchableOpacity>
+      <Text style={style.h1}> Botones </Text>
+      {/* Boton 1 */}
+      <TouchableOpacity style={style.botonBasico}>
+        <Text style={style.Texto}>Boton 1</Text>
+      </TouchableOpacity>
+      {/* Boton 2 */}
+      <TouchableOpacity style={style.botonIcono}>
+        <Ionicons name="user" size={20} style={style.iconB} />
+        <Text style={style.Text}> Boton Icono </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const style = StyleSheet.create({
-    mainS:{
-        flex:1,
-        backgroundColor: '#b8f3ebff',
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeigth:44,
-        padding: 50
-    },
-    h1: { fontSize: 25,
+  mainS: {
+    flex: 1,
+    backgroundColor: "#b8f3ebff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeigth : 44,
+    padding: 50,
+  },
+  h1: { fontSize: 25, fontWeight: "bold" },
+  botonBasico: {
+    backgroundColor: "#f5c5c5ff",
+    padding: 12,
+    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  Text: {
+    color: "#000000",
+    textAlign: "center",
     fontWeight: "bold",
   },
-    botonBasico:{
-      backgroundColor: '#f5c5c5ff',
-      padding: 12,
-      borderRadius: 10,
-      marginTop: 10,
-      marginBottom: 10,
-    }
-
- 
+  botonIcono: {
+    backgroundColor: "#fff",
+    fontSize: 30,
+    padding: 12,
+    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconB: {
+    color: "#a82c2cff",
+  },
 });
