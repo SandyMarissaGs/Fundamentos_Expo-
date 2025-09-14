56f4d7da174fae20851973a680822dfd7b96b1b0
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-09-14 13:23:08 -0600

    Creacion de AppBar

--- src/Pages/Lista-Alumnos/ListaAlumnos.js
+++ src/Pages/Lista-Alumnos/ListaAlumnos.js
@@ -1,17 +1,38 @@
-import { Platform, StatusBar, StyleSheet, View, Text } from "react-native";
+import { Platform,  SafeAreaView, StatusBar, StyleSheet, View, Text } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons"; 
 
 export const ListaAlumnos = () => {
   return (
-    <View style={style.mainS}>
-      <Text>Hola</Text>
-    </View>
+    <SafeAreaView style={style.mainS}>
+      {/*Inicio de AppBar */}
+      <View style={style.appBar}>
+        <MaterialIcons name="arrow-back" size={20} color="#000" />
+        <Text style={style.appBarTitle}> Lista de Usuarios</Text>
+        <View style={{width: 20}}></View>
+        </View>
+      {/*Fin de AppBar */}
+    </SafeAreaView>
   );
 };
 
 const style = StyleSheet.create({
   mainS: {
     flex: 1,
-    backgroundColor: "#a7f4eaff",
+    backgroundColor: "#f3f9f8ff",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 44,
+    //padding: 16,
+  },
+  appBar: {
+    height:50,
+    width: "100%",
+    backgroundColor: "#ab7aeaff",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+  },
+  appBarTitle: {
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
